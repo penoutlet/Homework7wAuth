a329bf96ceee60015d34786def1237a074e56dc7
penoutlet <sjonesmusician@gmail.com>
2017-03-08 11:04:57 -0500

    heroku

--- train.js
+++ train.js
@@ -13,6 +13,31 @@
     messagingSenderId: "650357396120"
   };
   firebase.initializeApp(config);
+
+// Authentication through github
+
+var provider = new firebase.auth.GithubAuthProvider();
+console.log("Provider: " + provider);
+// to get pop-up sign in:
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+
 // Save the database 
 var database = firebase.database();
 // var dtadded = firebase.database.ServerValue.TIMESTAMP;
